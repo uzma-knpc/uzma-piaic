9fb607a813d29b9f27a9c7aa2e281fef705c1f93
UZMAILYAS <135639485+uzma-knpc@users.noreply.github.com>
2024-04-03 16:19:48 +0500

    q11 modified

--- app11.js
+++ app11.js
@@ -1,9 +1,11 @@
 // Q11. Names in array
-var names = ["Hufsa", "aisha", "Ismail", "Yahya", "Imran"];
+var names1 = ["Hufsa", "aisha", "Ismail", "Yahya", "Imran"];
 // Print each person's name 
-names.array.forEach(function (names) {
-    console.log(names);
-});
+console.log(names1[0]);
+console.log(names1[1]);
+console.log(names1[2]);
+console.log(names1[3]);
+console.log(names1[4]);
 //names.forEach(name => {
 // console.log(name);
 //});
--- app11.ts
+++ app11.ts
@@ -2,9 +2,13 @@
 const names1:string [] = ["Hufsa", "aisha", "Ismail", "Yahya", "Imran"];
 
 // Print each person's name 
-names1Array.foreach(names1 => {
-   console.log(names1) 
-});
+
+   console.log(names1[0]) 
+   console.log(names1[1]) 
+   console.log(names1[2]) 
+   console.log(names1[3]) 
+   console.log(names1[4]) 
+
 //names.forEach(name => {
    // console.log(name);
 //});
