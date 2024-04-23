4bce1ac398dc12b08b9fd92647bb71b222afd540
UZMAILYAS <135639485+uzma-knpc@users.noreply.github.com>
2024-04-23 22:51:48 +0500

    Add files via upload
    40-41

---
+++ app40.ts
@@ -0,0 +1,32 @@
+// Album: Write a function called make_album()
+// function make_album(artist: string, title: string, tracks?: number) {
+    // const album: { artist: string, title: string, tracks?: number } = {
+        // artist: artist,
+        // title: title
+        //  let album= artist,title, tracks
+        // return 
+    // };
+    // if (tracks) {
+        // album.tracks = tracks;
+    // }
+    // return album;
+// }
+
+// Creating album objects
+// const album1 = make_album("Nusrat Fateh Ali Khan", "The Legend", 10);
+// const album2 = make_album("Junoon", "Azadi");
+// const album3 = make_album("Atif Aslam", "Doorie", 12);
+// Printing album information
+// console.log(album1);
+// console.log(album2);
+// console.log(album3);
+function song(ar:string,tt:string,tra:number) {
+
+    return (ar)
+}
+    // let response=eggrecp(1,1.5,3)
+    
+ var albumsong=song("JunaidJamshed","Late",17)
+ var albumsong2=song("atifaslam","legend",19)
+console.log("album1:",albumsong)
+console.log("album2:",albumsong2)
